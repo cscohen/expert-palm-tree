fb9c08f9fe589923d37af5ab57d5e635d58f0c77
Christopher Cohen <chris@cscohen.com>
2021-07-08 08:18:44 -0600

    testing onclick event

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
+function dogDown() {
+    document.getElementById('myDog').src = "harleyLook.jpeg";
+  }
+  function dogUp() {
+    document.getElementById('myDog').src = "harleyMouth.jpeg";
+  }
